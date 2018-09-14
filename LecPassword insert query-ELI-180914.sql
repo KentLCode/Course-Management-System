78da4eda254495493d4d807b409f8f1323bc4ea5
rmit-s3642823-Yixuan-Wang <42259386+rmit-s3642823-Yixuan-Wang@users.noreply.github.com>
2018-09-14 13:13:33 +1000

    Add files via upload

---
+++ LecPassword insert query-ELI-180914.sql
@@ -0,0 +1,6 @@
+insert into StfPassword(PersonId, UserName, Password) values
+(NULL, 'STF124574' ,'ABD234585'),
+(NULL, 'STF124575' ,'ABD234586'),
+(NULL, 'STF124576' ,'ABD234587'),
+(NULL, 'STF124577' ,'ABD234588'),
+(NULL, 'STF124578' ,'ABD234589');
