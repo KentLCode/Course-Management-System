4b51afde77d845d70a1e135c751ca2bfc5de46f2
rmit-s9905648-mike-little <32320747+rmit-s9905648-mike-little@users.noreply.github.com>
2018-10-02 20:18:36 +1000

    Add files via upload

---
+++ Sprint20180921/src/Control020AdminCMS.java
@@ -0,0 +1,78 @@
+// import what is required... here is an example
+//import java.util.Scanner;
+public class Control020AdminCMS
+{
+////////////////////////////////////////////////////////////////////////////////
+	/*--------------------------------------------------------------------------
+	 * This is a project of Team Invicibles, 
+	 * This team consisting of the following members:
+	 * 
+	 * Michael John Little:9905648@student.rmit.edu.au
+	 * Project Team Lead
+	 * Mob: +61 476 136 482
+	 * 
+	 * Bingnan (Kent) Li: s3699455@student.rmit.edu.au 
+	 * Project Team member
+	 * 
+	 * Yixuan (Eli) Wang:s3642823@student.rmit.edu.au 
+	 * Project Team member
+	 * 
+
+	 * 
+	 * -------------------------------------------------------------------------
+	 * About the operation of this class and main method:
+	 * a.  
+	 * b.  
+	 * c. 
+	 * 
+	/*--------------------------------------------------------------------------
+	 * 
+	 */
+	
+	////////////////////////////////////////////////////////////////////////////////
+
+	//Define Class Attributes[Variables] Here
+
+	// Here are the class variables for the:
+	// 1. This ...
+	// 2. This corresponds to SUC....
+	// 3. This ...
+	// 4. This ...
+
+	
+	 ///////////////////////////////////////////////////////////////////////////
+	 //Define Class Methods from here
+	//=========================================================================
+		// Define the constructor method
+		/*
+		*  <<class name>>()
+		*/		 
+		public Control020AdminCMS()  
+		{
+			// This is the constructor for <<class name>>()
+			// Allows access to the class methods.
+
+		}// close <<class name>>() 	
+
+ 
+		
+		//=========================================================================
+		// Define the methodDefineName() method
+		/*
+		*  ..()
+		*   This methodDefineName() method ... 
+		*	is a templated method..
+		*  ...
+		*/
+		//public  methodDefineName() 
+		{
+			// insert code from here...
+			//return userName;
+		}// end methodDefineName()
+
+		
+		
+		
+		
+////////////////////////////////////////////////////////////////////////////////
+}// <<class name>> class 
